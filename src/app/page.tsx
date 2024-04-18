2dc54cd659a0c985ba10981b50ce081901d09e56
vladislav kotelnikov <vladislav.kotelnikov@internet.ru>
2024-04-18 20:37:24 +0300

    Стилизован компонет Typography

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import Link from 'next/link';
-import {
-    getRouteMainPage,
-    getRouteOrderEquipmentPage,
-    getRouteOrderServicePage,
-} from 'src/entry';
 import { Typography } from 'src/shared';
 
 /**
@@ -12,16 +6,67 @@ import { Typography } from 'src/shared';
 export default function Home() {
     return (
         <div>
-            <Typography tag={'h1'}>Hello Next.js</Typography>
-            <Link href={getRouteMainPage()}>
-                <Typography tag={'p'}>Главная страница</Typography>
-            </Link>
-            <Link href={getRouteOrderServicePage()}>
-                <Typography tag={'p'}>Заказ услуги</Typography>
-            </Link>
-            <Link href={getRouteOrderEquipmentPage()}>
-                <Typography tag={'p'}>Заказ техники</Typography>
-            </Link>
+            <hr />
+            <p>light</p>
+            <Typography tag={'h1'}>Заголовок первого уровня</Typography>
+            <Typography tag={'h2'}>Заголовок второго уровня</Typography>
+            <Typography tag={'h3'}>Заголовок третьего уровня</Typography>
+            <Typography tag={'h4'}>Заголовок четвертого уровня</Typography>
+            <Typography tag={'h5'}>Заголовок пятого уровня</Typography>
+            <Typography tag={'h6'}>Заголовок шестого уровня</Typography>
+            <Typography tag={'p'}>Параграф</Typography>
+            <Typography tag={'span'}>Параграф</Typography>
+            <hr />
+
+            <hr />
+            <p>darkened</p>
+            <Typography tag={'h1'} color={'darkened'}>
+                Заголовок первого уровня
+            </Typography>
+            <Typography tag={'h2'} color={'darkened'}>
+                Заголовок второго уровня
+            </Typography>
+            <Typography tag={'h3'} color={'darkened'}>
+                Заголовок третьего уровня
+            </Typography>
+            <Typography tag={'h4'} color={'darkened'}>
+                Заголовок четвертого уровня
+            </Typography>
+            <Typography tag={'h5'} color={'darkened'}>
+                Заголовок пятого уровня
+            </Typography>
+            <Typography tag={'h6'} color={'darkened'}>
+                Заголовок шестого уровня
+            </Typography>
+            <Typography tag={'p'} color={'darkened'}>
+                Параграф
+            </Typography>
+            <hr />
+
+            <hr />
+            <p>muted</p>
+            <Typography tag={'h1'} color={'muted'}>
+                Заголовок первого уровня
+            </Typography>
+            <Typography tag={'h2'} color={'muted'}>
+                Заголовок второго уровня
+            </Typography>
+            <Typography tag={'h3'} color={'muted'}>
+                Заголовок третьего уровня
+            </Typography>
+            <Typography tag={'h4'} color={'muted'}>
+                Заголовок четвертого уровня
+            </Typography>
+            <Typography tag={'h5'} color={'muted'}>
+                Заголовок пятого уровня
+            </Typography>
+            <Typography tag={'h6'} color={'muted'}>
+                Заголовок шестого уровня
+            </Typography>
+            <Typography tag={'p'} color={'muted'}>
+                Параграф
+            </Typography>
+            <hr />
         </div>
     );
 }
--- src/shared/components/Typography/Typography.tsx
+++ src/shared/components/Typography/Typography.tsx
@@ -2,17 +2,23 @@ import React from 'react';
 import cn from 'classnames';
 import styles from './style.module.scss';
 
-type TTypographyTag = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p';
+type TTypographyTag = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span';
 
 interface ITypographyProps
     extends React.HTMLProps<HTMLElementTagNameMap[TTypographyTag]> {
     tag: TTypographyTag;
+    color?: 'light' | 'darkened' | 'muted';
 }
 
 /**
  * Типография сайта
  * */
-export const Typography = ({ tag, className, ...props }: ITypographyProps) => {
+export const Typography = ({
+    tag,
+    className,
+    color = 'light',
+    ...props
+}: ITypographyProps) => {
     const Element = tag as React.ElementType;
 
     return (
@@ -20,7 +26,8 @@ export const Typography = ({ tag, className, ...props }: ITypographyProps) => {
             className={cn(
                 className,
                 styles['typography'],
-                styles[`typography-${tag}`]
+                styles[`typography-${tag}`],
+                styles[`typography--${color}`]
             )}
             {...props}
         />
