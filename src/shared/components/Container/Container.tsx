5d314c80af0c652b13d403b532f2482784aa6693
kotelnikov-vladislav <vladislav.kotelnikov@internet.ru>
2024-06-23 13:18:00 +0300

    main: реализовано автоматическое уменьшие размера шрифта в зависимости от ширины контейнера

--- src/shared/components/Container/Container.tsx
+++ src/shared/components/Container/Container.tsx
@@ -1,6 +1,20 @@
-import { HTMLAttributes, ReactNode } from 'react';
+'use client';
+
+import React, {
+    createRef,
+    HTMLAttributes,
+    ReactNode,
+    RefObject,
+    useEffect,
+    useRef,
+    useState,
+} from 'react';
 import cn from 'classnames';
 import styles from './style.module.scss';
+
+const FONT_SIZE_BASE = 10; // px
+const FONT_SIZE_MIN = 8; // px
+const BASE_WIDTH_CONTAINER = 1140; // px
 
 interface IContainerProps extends HTMLAttributes<HTMLDivElement> {}
 
@@ -8,5 +22,36 @@ interface IContainerProps extends HTMLAttributes<HTMLDivElement> {}
  * Ограничивает ширину содержимого
  * */
 export const Container = ({ className, ...props }: IContainerProps) => {
-    return <div className={cn(className, styles['container'])} {...props} />;
+    const ref = createRef<HTMLDivElement>();
+
+    const onResizeContainerHandler = (ref: RefObject<HTMLDivElement>) => {
+        if (!ref.current) return;
+        const width = ref.current.getBoundingClientRect().width;
+
+        const newFontSize = Math.max(
+            FONT_SIZE_MIN,
+            (width * FONT_SIZE_BASE) / BASE_WIDTH_CONTAINER
+        );
+
+        document.documentElement.style.fontSize = `${newFontSize}px`;
+    };
+
+    useEffect(() => {
+        onResizeContainerHandler(ref);
+        window.addEventListener('resize', () => onResizeContainerHandler(ref));
+
+        return () => {
+            window.removeEventListener('resize', () =>
+                onResizeContainerHandler(ref)
+            );
+        };
+    }, [ref]);
+
+    return (
+        <div
+            ref={ref}
+            className={cn(className, styles['container'])}
+            {...props}
+        />
+    );
 };
