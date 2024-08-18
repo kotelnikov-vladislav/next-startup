7b3b32a888a6454bd0dd2a33927098ecd5d05e37
kotelnikov-vladislav <vladislav.kotelnikov@internet.ru>
2024-08-18 17:22:13 +0300

    main: базовый адаптив секции 'наши услуги'

--- src/shared/components/HorizCard/HorizCard.tsx
+++ src/shared/components/HorizCard/HorizCard.tsx
@@ -3,37 +3,42 @@ import { Typography } from 'src/shared';
 import styles from './style.module.scss';
 
 interface IHorizCardProps {
-    title: string;
-    imageUrl: string;
-    description: string;
+	title: string;
+	imageUrl: string;
+	description: string;
 }
 
 /**
  * Горизонтальная карточка
  * */
 export const HorizCard = ({
-    title,
-    imageUrl,
-    description,
+	title,
+	imageUrl,
+	description,
 }: IHorizCardProps) => {
-    return (
-        <div className={styles['service-card']}>
-            <Images
-                alt={title}
-                width={'80'}
-                height={'80'}
-                className={styles['service-card__image']}
-                src={`/images/services/${imageUrl}.png`}
-            />
-            <Typography tag={'h6'} className={styles['service-card__title']}>
-                {title}
-            </Typography>
-            <Typography
-                tag={'p'}
-                className={styles['service-card__description']}
-            >
-                {description}
-            </Typography>
-        </div>
-    );
+	return (
+		<div className={styles['service-card']}>
+			<Images
+				alt={title}
+				width={'48'}
+				height={'48'}
+				className={styles['service-card__image']}
+				src={`/images/services/${imageUrl}.png`}
+			/>
+			<div>
+				<Typography
+					tag={'h6'}
+					className={styles['service-card__title']}
+				>
+					{title}
+				</Typography>
+				<Typography
+					tag={'p'}
+					className={styles['service-card__description']}
+				>
+					{description}
+				</Typography>
+			</div>
+		</div>
+	);
 };
