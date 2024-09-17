0ca2ab6549d6782ae93a75f724303c6e9f6a5e24
kotelnikov-vladislav <vladislav.kotelnikov@internet.ru>
2024-09-17 23:47:08 +0300

    main: изменено лого

--- src/shared/components/Logo/Logo.tsx
+++ src/shared/components/Logo/Logo.tsx
@@ -1,23 +1,27 @@
 import Image from 'next/image';
 import styles from './style.module.scss';
 import { Typography } from 'src/shared';
-import { getRouteMainPage } from '../../../entry';
 import Link from 'next/link';
+import { getRouteMainPage } from 'src/entry';
 
 export const Logo = () => {
-    return (
-        <Link href={getRouteMainPage()} style={{ textDecoration: 'none' }}>
-            <div className={styles['logo']}>
-                <Image
-                    width={40}
-                    height={40}
-                    src={'/images/commons/logo.png'}
-                    alt={'Логотип компании агробота'}
-                />
-                <Typography tag={'h2'} color={'darkened'}>
-                    AgroBot
-                </Typography>
-            </div>
-        </Link>
-    );
+	return (
+		<Link href={getRouteMainPage()} style={{ textDecoration: 'none' }}>
+			<div className={styles['logo']}>
+				<Image
+					width={40}
+					height={40}
+					src={'/images/commons/logo.png'}
+					alt={'Логотип компании агробота'}
+				/>
+				<Typography
+					tag={'h3'}
+					color={'darkened'}
+					className={styles.logo__text}
+				>
+					борщевик.нет
+				</Typography>
+			</div>
+		</Link>
+	);
 };
