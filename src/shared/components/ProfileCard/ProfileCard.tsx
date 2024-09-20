d8090c8a613c2f4d6229cd5e652251c6616df45f
kotelnikov-vladislav <vladislav.kotelnikov@internet.ru>
2024-09-20 20:15:42 +0300

    main: доработан адаптив секции команды

--- src/shared/components/ProfileCard/ProfileCard.tsx
+++ src/shared/components/ProfileCard/ProfileCard.tsx
@@ -4,74 +4,78 @@ import { Typography } from 'src/shared';
 import styles from './style.module.scss';
 
 interface IProfileCardProps {
-    title: string;
-    imageUrl: string;
-    description: string;
-    socNetwork: {
-        vk?: string;
-        tg?: string;
-    };
+	title: string;
+	imageUrl: string;
+	description: string;
+	socNetwork: {
+		vk?: string;
+		tg?: string;
+	};
 }
 
 /**
  * Карточка профиля
  * */
 export const ProfileCard = ({
-    title,
-    imageUrl,
-    description,
-    socNetwork,
+	title,
+	imageUrl,
+	description,
+	socNetwork,
 }: IProfileCardProps) => {
-    return (
-        <div className={styles['profile-card']}>
-            <Images
-                alt={title}
-                width={360}
-                height={420}
-                src={imageUrl}
-                className={styles['profile-card__image']}
-            />
-            <Typography tag={'h6'} className={styles['profile-card__title']}>
-                {title}
-            </Typography>
-            <Typography
-                tag={'p'}
-                className={styles['profile-card__description']}
-            >
-                {description}
-            </Typography>
+	return (
+		<div className={styles['profile-card']}>
+			<Images
+				alt={title}
+				width={300}
+				height={350}
+				src={imageUrl}
+				className={styles['profile-card__image']}
+			/>
+			<Typography
+				tag={'h6'}
+				title={title}
+				className={styles['profile-card__title']}
+			>
+				{title}
+			</Typography>
+			<Typography
+				tag={'p'}
+				className={styles['profile-card__description']}
+			>
+				{description}
+			</Typography>
 
-            <div className={styles['profile-card__soc-networks']}>
-                {socNetwork.vk && (
-                    <a
-                        title={'ВКонтакте'}
-                        target={'_blank'}
-                        href={socNetwork.vk}
-                        className={styles['profile-card__soc']}
-                    >
-                        <Images
-                            width={40}
-                            height={40}
-                            alt={'Вконтакте'}
-                            src={'/images/soc-networks/vk.svg'}
-                        />
-                    </a>
-                )}
-                {socNetwork.tg && (
-                    <a
-                        target={'_blank'}
-                        href={socNetwork.tg}
-                        className={styles['profile-card__soc']}
-                    >
-                        <Images
-                            width={40}
-                            height={40}
-                            alt={'Telegram'}
-                            src={'/images/soc-networks/telegram.svg'}
-                        />
-                    </a>
-                )}
-            </div>
-        </div>
-    );
+			<div className={styles['profile-card__soc-networks']}>
+				{socNetwork.vk && (
+					<a
+						title={'ВКонтакте'}
+						target={'_blank'}
+						href={socNetwork.vk}
+						className={styles['profile-card__soc']}
+					>
+						<Images
+							width={24}
+							height={24}
+							alt={'Вконтакте'}
+							src={'/images/soc-networks/vk.svg'}
+						/>
+					</a>
+				)}
+				{socNetwork.tg && (
+					<a
+						target={'_blank'}
+						href={socNetwork.tg}
+						className={styles['profile-card__soc']}
+					>
+						<Images
+							width={24}
+							height={24}
+							alt={'Telegram'}
+							src={'/images/soc-networks/telegram.svg'}
+						/>
+					</a>
+				)}
+			</div>
+		</div>
+	);
 };
